748bab136ebf7d9c1d1e631c3273c19fbbfcba52
BiMia <150850183+BiMia@users.noreply.github.com>
2024-04-17 22:03:50 +0100

    homework7 javascript

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = galleryItems.map(item => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img 
+      class="gallery__image" 
+      src="${item.preview}" 
+      data-source="${item.original}" 
+      alt="${item.description}" />
+    </a>
+  </li>
+`).join("");
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+
+galleryContainer.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+
+  const source = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">
+  `);
+  event.preventDefault();
+  instance.show();
+});
+
+galleryContainer.addEventListener('keyup', event => {
+    if(event.code === 'Escape' && basicLightbox.visible()) {
+        instance.close();
+    }
+});
+
 console.log(galleryItems);
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+function createGalleryMarkup(items) {
+    return items.map(item => `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+            </a>
+        </li>
+    `).join("");
+}
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+document.querySelector('.gallery').innerHTML = galleryMarkup;
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom'
+});
 
 console.log(galleryItems);
